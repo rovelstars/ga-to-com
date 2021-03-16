fb4ccb1196a8830717f30757c8df6737d567bda0
sayantan300 <rovelstars300@gmail.com>
2021-03-16 12:27:05 +0530

    add 301 status for permanent redirection

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require("express");
 var app = express();
 
 var domain = function(req, res) {
- if (req.hostname == "bots.rovelstars.ga") res.redirect(`https://discord.rovelstars.com${req.path}`);
+ if (req.hostname == "bots.rovelstars.ga") res.redirect(301, `https://discord.rovelstars.com${req.path}`);
 
- if (req.hostname == "rovelstars.ga") res.redirect(`https://docs.rovelstars.com${req.path}`);
+ if (req.hostname == "rovelstars.ga") res.redirect(301, `https://docs.rovelstars.com${req.path}`);
 
- if (req.hostname == "paste.rovelstars.ga") res.redirect(`https://paste.rovelstars.com${req.path}`);
+ if (req.hostname == "paste.rovelstars.ga") res.redirect(301, `https://paste.rovelstars.com${req.path}`);
 
- if(req.hostname=="api.rovelstars.ga") res.redirect(`https://api.rovelstars.com${req.path}`);
+ if(req.hostname=="api.rovelstars.ga") res.redirect(301, `https://api.rovelstars.com${req.path}`);
 
- if(req.hostname=="blog.rovelstars.ga") res.redirect(`https://blog.rovelstars.com${req.path}`);
+ if(req.hostname=="blog.rovelstars.ga") res.redirect(301, `https://blog.rovelstars.com${req.path}`);
 
  else{
-  res.send("Are you the one that was lost in space?");
+  res.send(404, "Are you the one that was lost in space?");
  }
 }
 app.use(domain);
